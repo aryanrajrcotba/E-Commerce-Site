94097b59c626768ab433b2a1eb90a8944abbd92a
Aryan Raj <aryanraj19770@gmail.com>
2025-05-18 15:26:19 +0530

    Final Changes

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
-MYSQL_HOST = 'localhost'
-MYSQL_USER = 'root'  
-MYSQL_PASSWORD = 'Patna@1234'  
-MYSQL_DB = 'ecommerce'
+import os
+
+MYSQL_HOST = os.getenv('MYSQL_HOST', 'localhost')
+MYSQL_USER = os.getenv('MYSQL_USER', 'root')
+MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', '')
+MYSQL_DB = os.getenv('MYSQL_DB', 'ecommerce')
